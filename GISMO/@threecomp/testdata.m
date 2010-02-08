05a52588e0f21a9dc26e4783489d250da0e2c6a8
mewest@gmail.com <mewest@gmail.com@b512c968-07be-11df-bdae-5552c88b53c9>
2010-02-08 08:02:31 +0000

    Added threecomp object.

---
+++ GISMO/@threecomp/testdata.m
@@ -0,0 +1,17 @@
+% LOAD TEST DATA
+
+
+
+whichgismo('WestPCBranch');
+clear classes
+load load_traces
+w1 = [w31300127];
+w = [
+   w31300127
+   w31300800
+   w31301753
+   w31300192
+];
+TC = ThreeComp(w);
+
+
